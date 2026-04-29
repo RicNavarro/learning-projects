dbdaf568dd762811dda88d30bfd466ab320f5474
RicNavarro <ricardonavarro2611@gmail.com>
2026-04-29 07:59:34 -0300

    Configurando requisilçoes "POST"

--- dotnetting/orderflow-api/OrderFlow.Api/Controllers/ClientController.cs
+++ dotnetting/orderflow-api/OrderFlow.Api/Controllers/ClientController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using OrderFlow.Api.Models;
 
 namespace OrderFlow.Api.Controllers
 {
@@ -6,17 +7,32 @@ namespace OrderFlow.Api.Controllers
     [Route("api/[controller]")]
     public class ClientController : ControllerBase
     {
+
+        private static List<Client> clients = new List<Client>();
+
         [HttpGet]
         public IActionResult GetClients()
         {
-            var clients = new List<string>
-            {
-                "Ricardo",
-                "Jayane",
-                "Yelena"
-            };
+            //var clients = new List<string>
+            //{
+            //    "Ricardo",
+            //    "Jayane",
+            //    "Yelena"
+            //};
 
             return Ok(clients);
         }
+
+        [HttpPost]
+        public IActionResult CreateClient(Client client)
+        {
+            client.Id = clients.Count + 1;
+
+            clients.Add(client);
+
+            return Ok(client);
+        }
+
+
     }
 }
---
+++ dotnetting/orderflow-api/OrderFlow.Api/Models/Client.cs
@@ -0,0 +1,11 @@
+namespace OrderFlow.Api.Models
+{
+    public class Client
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+    }
+}
