06dfe181eb938eedce7a9775f2839faa5ee93f5b
RicNavarro <ricardonavarro2611@gmail.com>
2026-06-30 09:58:52 -0300

    Validação de tamanho das requisições

---
+++ dotnetting/orderflow-api/OrderFlow.Api/DTOs/Validators/GetOrdersRequestValidator.cs
@@ -0,0 +1,22 @@
+using FluentValidation;
+using OrderFlow.Api.DTOs.Requests;
+
+namespace OrderFlow.Api.DTOs.Validators;
+
+public class GetOrdersRequestValidator : AbstractValidator<GetOrdersRequest>
+{
+    public GetOrdersRequestValidator()
+    {
+        RuleFor(x => x.Page)
+            .GreaterThan(0)
+            .WithMessage("A página deve ser maior que zero.");
+
+        RuleFor(x => x.PageSize)
+            .GreaterThan(0)
+            .WithMessage("O tamanho da página deve ser maior que zero.");
+
+        RuleFor(x => x.PageSize)
+            .LessThanOrEqualTo(100)
+            .WithMessage("O tamanho máximo da página é 100.");
+    }
+}
